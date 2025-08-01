c870d1b0ef07c0ea05e140894feb26d0e6f1451d
Anya <81263768+anyabuu@users.noreply.github.com>
2025-08-01 19:17:49 +0300

    added message for login

--- src/routes/user/loginUser.js
+++ src/routes/user/loginUser.js
@@ -24,6 +24,7 @@ export const loginUserRoute = async (request, reply) => {
   const token = await userService.loginUser(currentUser._id);
 
   reply.status(201).send({
+    message: 'Successful login',
     token,
   });
 };
